70d471b9b0bf70e8ebebad49ac058b4eb55fdb9c
AleksandraGubanova <Tragedienne@users.noreply.github.com>
2023-06-30 19:53:01 +0300

    Initial commit

---
+++ src/task2/Btn_click.java
@@ -0,0 +1,38 @@
+package task2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Btn_click {
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+        String appUrl = "https://games.logicplays.com/red-button/";
+
+        driver.get(appUrl);
+
+        driver.manage().window().maximize();
+
+        String expectedTitle = "Красная кнопка";
+        String actualTitle = driver.getTitle();
+
+        if (expectedTitle.equals(actualTitle))
+        {
+            System.out.println("Verification Successful.");
+        }
+        else
+        {
+            System.out.println("Verification Failed.");
+        }
+
+        WebElement Button = driver.findElement(By.className("button"));
+        Button.click();
+
+        driver.close();
+        System.out.println("Test script executed successfully.");
+
+        System.exit(0);
+    }
+}
